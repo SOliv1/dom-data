c5f787b480ef4cb91fc084bbf47542b3a80be62f
Ubuntu <ubuntu@ip-172-31-95-238.ec2.internal>
2019-09-12 08:35:08 +0000

    adding xhr

---
+++ ajax-script/main.js
@@ -0,0 +1,16 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+
+    if (this.readyState == 4 && this.status == 200) {
+
+        document.getElementById("data").innerHTML = this.responseText;
+
+    }
+
+};
+
+
+xhr.open("GET", "https://swapi.co/api/");
+
+
+xhr.send();
