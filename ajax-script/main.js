7050e60a59d67a978b1d637032b485667bc6c79e
Ubuntu <ubuntu@ip-172-31-95-238.ec2.internal>
2019-09-12 11:40:53 +0000

    adding json.parse directly into console

--- ajax-script/main.js
+++ ajax-script/main.js
@@ -19,7 +19,7 @@ xhr.onreadystatechange = function() {
 
     if (this.readyState == 4 && this.status == 200) {
 
-        console.log(typeof(JSON.parse(this.responseText)));
+        console.log(JSON.parse(this.responseText));
 
     };
 
