e7fc02a4137da760d1ea5a33afe1a2f63b22c655
Ubuntu <ubuntu@ip-172-31-95-238.ec2.internal>
2019-09-12 13:14:31 +0000

    getting dom data on page

--- ajax-script/main.js
+++ ajax-script/main.js
@@ -1,30 +1,32 @@
-function getData(cb) {
-
+const baseURL = "https://swapi.co/api/";
+function getData(type, cb) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://swapi.co/api/");
-
-    xhr.send();
 
 
     xhr.onreadystatechange = function() {
 
         if (this.readyState == 4 && this.status == 200) {
-
             cb(JSON.parse(this.responseText));
 
         }
 
     };
 
-}
+    xhr.open("GET", baseURL + type + "/");
 
-
-
-function printDataToConsole(data) {
-
-    console.log(data);
+    xhr.send();
 
 }
 
-getData(printDataToConsole);
+
+
+function writeToDocument(type) {
+
+    getData(type, function(data) {
+
+        document.getElementById("data").innerHTML = data;
+
+    });
+
+}
