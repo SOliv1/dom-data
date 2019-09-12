ada3a29e3733391b8ad554b11c875b12207b7889
Ubuntu <ubuntu@ip-172-31-95-238.ec2.internal>
2019-09-12 11:32:33 +0000

    adding json parse

--- ajax-script/main.js
+++ ajax-script/main.js
@@ -1,16 +1,29 @@
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
 
-    if (this.readyState == 4 && this.status == 200) {
-
-        document.getElementById("data").innerHTML = this.responseText;
-
-    }
-
-};
+var data;
 
 
 xhr.open("GET", "https://swapi.co/api/");
 
-
 xhr.send();
+
+
+
+function setData(jsonData) {
+
+    data = jsonData;
+
+}
+
+xhr.onreadystatechange = function() {
+
+    if (this.readyState == 4 && this.status == 200) {
+
+        console.log(typeof(JSON.parse(this.responseText)));
+
+    };
+
+}
+
+
+
