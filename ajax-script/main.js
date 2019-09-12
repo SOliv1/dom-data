e3bd05daa8097468d6d1b87fbc077c781326e334
Ubuntu <ubuntu@ip-172-31-95-238.ec2.internal>
2019-09-12 12:06:46 +0000

    desirealization

--- ajax-script/main.js
+++ ajax-script/main.js
@@ -1,22 +1,21 @@
 var xhr = new XMLHttpRequest();
+
 var data;
 
-xhr.open("GET", "https://swapi.co/api/");
-xhr.send();
 
-function setData(jsonData) {
-}
+
+xhr.open("GET", "https://swapi.co/api/");
+
+xhr.send();
 
 xhr.onreadystatechange = function() {
 
     if (this.readyState == 4 && this.status == 200) {
 
-            data = this.responseText;
-            console.log(data);
+        data = this.responseText;
 
     };
 
 }
 
-
-
+console.log(data);
